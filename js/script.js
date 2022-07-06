79b37ae5cb4652cacc58336bacc48c06bfe3b93c
Anna O <usune@mail.ru>
2022-07-07 02:34:23 +0300

    modul05_lesson07

--- js/script.js
+++ js/script.js
@@ -2,18 +2,16 @@
 
 const addProduct = document.querySelector('.list-product__table-add');
 const tBody = document.querySelector('.list-product__table-body');
-
-const modalTitle = document.querySelector('.add-product__title');
-const productId = document.querySelectorAll('.add-product__id')[1];
-
-const buttonId = document.querySelector('.add-product__id-button');
-const formOverlay = document.querySelector('.overlay');
-const productForm = document.querySelector('.add-product__form');
-const inputId = productForm.querySelectorAll('.add-product__input')[0];
-
-const productSum = formOverlay.querySelector('.card-sum__price');
 const allProductSum = document.querySelector('.list-product .card-sum__price');
 
+const formOverlay = document.querySelector('.overlay');
+const modalTitle = formOverlay.querySelector('.add-product__title');
+const productId = formOverlay.querySelectorAll('.add-product__id')[1];
+const buttonId = formOverlay.querySelector('.add-product__id-button');
+const productSum = formOverlay.querySelector('.card-sum__price');
+
+const productForm = formOverlay.querySelector('.add-product__form');
+const inputId = productForm.querySelectorAll('.add-product__input')[0];
 const checkbox = productForm.querySelector(".add-product__checkbox");
 const checkInput = productForm.querySelectorAll(".add-product__input")[4];
 
